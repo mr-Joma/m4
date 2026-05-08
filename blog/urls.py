12090e3f97837d4bd022c9a2373c88b53924faa6
mr-Joma <jomaturgunbekov@gmail.com>
2026-05-09 00:55:59 +0600

    lesson4

--- blog/urls.py
+++ blog/urls.py
@@ -19,14 +19,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from posts.views import get_post, get_posts_by_category, home, post
+from posts.views import get_post, home, post, get_posts_by_category, create_post
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", home, name="home"),
-    path("posts/", post, name="posts"),
-    path("posts/<int:id>/", get_post, name="post"),
+    path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('posts/', post, name="posts"),
+    path('posts/<int:id>/', get_post, name="post"),
     path("posts/category/<int:id>/", get_posts_by_category, name="category"),
+    path("posts/create", create_post, name="create_post"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest
+from django.shortcuts import render, get_object_or_404, redirect
 
-from posts.models import Post
+from posts.forms import PostForm
+from posts.models import Post, Category
 from posts.posts import get_posts_filter_by_rate
 
 def home(request):
@@ -28,3 +29,60 @@ def posts(request):
     )
 
     return render(request, 'posts/posts.html', {'posts': posts})
+
+
+def create_post(request: HttpRequest):
+
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect("posts")
+
+        return render(request, "posts/create_post.html", context={"error": form.errors})
+    
+        # title = request.POST.get("title", "").strip()
+        # content = request.POST.get("content", "")
+        # rate = request.POST.get("rate", 0)
+        # image = request.FILES.get("image")
+
+        # banned_words = ["plohoe slovo", "ban", "INSERT", "SELECT"]
+
+        # if title in banned_words:
+        #     return render(
+        #         request,
+        #         "posts/create_post.html",
+        #         context={
+        #             "error": "Нельзя создать пост с таким названием!",
+        #             "title": title,
+        #             "content": content,
+        #             "rate": rate,
+        #             "image": image,
+        #         },
+        #     )
+
+        # if not title or not content or not rate:
+        #     return render(
+        #         request,
+        #         "posts/create_post.html",
+        #         context={
+        #             "error": "Нельзя создать пустой пост!",
+        #             "title": title,
+        #             "content": content,
+        #             "rate": rate,
+        #             "image": image,
+        #         },
+        #     )
+        # Post.objects.create(title=title, content=content, rate=rate, image=image)
+    
+
+    form = PostForm()
+
+    categories = Category.objects.all()
+
+    return render(
+        request,
+        "posts/create_post.html",
+        context={"form": form, "categories": categories},
+    )
